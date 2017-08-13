de3fcf97b7915bdb12af4a902a536b58ba794640
mariasvenson <mariasvenson@yahoo.se>
2017-08-13 22:20:20 +0200

    new file

---
+++ PhoneList.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.util.Scanner;
+
+
+public class PhoneList{
+	public static void main(String[] args) {  
+
+	//Scanner sc = new Scanner(System.in);
+	String input = "2\n3\n911\n97625999\n91125426\n5\n113\n12340\n123440\n12345\n98346\n";
+	Scanner sc = new Scanner(input);
+    String str = sc.nextLine();
+
+	}
+
+}
