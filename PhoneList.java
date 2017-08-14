0400dd5126d15d185382d3533819fee86c984abd
mariasvenson <mariasvenson@yahoo.se>
2017-08-14 21:23:26 +0200

    added some code

--- PhoneList.java
+++ PhoneList.java
@@ -7,11 +7,21 @@ import java.util.Scanner;
 public class PhoneList{
 	public static void main(String[] args) {  
 
-	//Scanner sc = new Scanner(System.in);
-	String input = "2\n3\n911\n97625999\n91125426\n5\n113\n12340\n123440\n12345\n98346\n";
+	Scanner sc = new Scanner(System.in);
+	//String input = "2\n3\n911\n97625999\n91125426\n5\n113\n12340\n123440\n12345\n98346\n";
 	Scanner sc = new Scanner(input);
     String str = sc.nextLine();
+    int numTimes = Integer.parseInt(str);
 
+    for(int i = 0; i < numTimes; i++){
+    	int nrNumbers = sc.nextLine();
+    	for(int j = 0; j < nrNumbers; j++){
+    		String[] allNumbers = new String []
+
+    	}
+    }
+
+	
 	}
 
 }
