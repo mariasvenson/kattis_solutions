f64312891157b339f069f1d4f62999f00f3043d8
mariasvenson <mariasvenson@yahoo.se>
2017-08-16 22:54:28 +0200

    work in progress

--- VirtualFriends.java
+++ VirtualFriends.java
@@ -2,59 +2,108 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 
-public class Node{
+
+public class VirtualFriends{
+
+	public static class Node{
 	String name;	
-	ArrayList<Node> edges;
+	ArrayList<Node> neighbours;
 
-	public addEdge(Node n){
-		this.edges.add(edge);
-		n.edges.add(this)
-	}
-	
-	}
+		public Node(String name){
+			this.name = name;
+			this.neighbours = new ArrayList<Node>();
+		}
 
-
-public class Edge{
-	Node n1;
-	Node n2; 
-
-	public Edge(Node n1, Node n2){
-		this.n1 = n1; 
-		this.n2 = n2;
+		public void addNeighbour(Node neighbour){
+			this.neighbours.add(neighbour);
+			//neighbour.neighbours.add(this);
+		}
 	}
 
-	public Edge(String name1, String n2){
-		this.n1 = new Node(name1); 
-		this.n2 = new Node(name2);
-	}
+	public static void main(String[] args) {  
 
+	String input = "1\n3\nFred Barney\nBarney Betty\nBetty Wilma\n"; 
+    Scanner sc = new Scanner(input);
+	//Scanner sc = new Scanner(System.in);
+	int numCases = Integer.parseInt(sc.nextLine());
+	for(int i = 0; i < numCases; i++){
+		int numPeople = Integer.parseInt(sc.nextLine());
+		
+		for(int j = 0; j < numPeople; j++){
+			String[] names = sc.nextLine().split(" ");
+			ArrayList<Node> network = new ArrayList<Node>();
+			String name1 = names[0];
+			String name2 = names[1];
+			Node node1;
+			Node node2;
+
+			if(nodeExist(name1,network) == false){
+				node1 = new Node(name1);
+				network.add(node1);
+			}else{
+				node1 = getNode(name1, network);
+
+			}
+			if(nodeExist(name2,network) == false){
+				node2 = new Node(name2);
+				network.add(node2);
+			}else{
+				node2 = getNode(name2, network);
+			}
+
+			node1.addNeighbour(node2);
+			node2.addNeighbour(node1);
+
+			int count = 0; 
+			ArrayList<Node> queue = new ArrayList<Node>();
+			ArrayList<String> visited = new ArrayList<String>();
+			queue.add(node1);
+			
+			System.out.println("-------------");
+			System.out.println(node1.name);
+			for(Node u : node1.neighbours){
+				System.out.println(u.name);
+			}
+			while(queue.isEmpty() == false){
+				Node currentNode = queue.remove(0);
+				//System.out.println(currentNode.name);
+				if(visited.contains(currentNode.name)){
+					continue; 
+				}
+				count++;
+				visited.add(currentNode.name);
+				for(Node n : currentNode.neighbours){
+					if(visited.contains(n)){
+					}else{
+						queue.add(n);
+					}
+				}	
+			}
+
+		System.out.println(count);
+
+		}
+
+	}
 }
 
-public class Node{
-	String name; 
 
-	public Node(String name){
-		this.name = name;
+	public static boolean nodeExist(String name, ArrayList<Node> network){
+		for(Node n : network){
+			if(n.name.equals(name)){
+				return true;
+			}
+		}
+		return false; 
+	}
+
+	public static Node getNode(String name, ArrayList<Node> network){
+		for(Node node : network){
+			if(node.name.equals(name)){
+				return node; 
+			}
+		}
+		return null;
 	}
 }
-
-public class Graph{
-
-	ArrayList<Edge> edges; 
-
-	public Graph(){
-		this.edges = new ArrayList<Edge>();
-	}
-
-	public void addEdge(Node n1, Node n2){
-		Edge edge = new Edge(n1,n2);
-		edges.add(edge);
-	}
-}
-
-
-
-
-
-
 
